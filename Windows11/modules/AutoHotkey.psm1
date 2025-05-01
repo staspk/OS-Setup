01c5d5e01e93903b95da84cc342db7e915274374
Stanislav Kozubenko <staspk@gmail.com>
2025-05-01 15:52:19 -0700

    Automatic Settings Backup

--- Windows11/modules/AutoHotkey.psm1
+++ Windows11/modules/AutoHotkey.psm1
@@ -1,6 +1,13 @@
-$HOTKEYS = "$PWD\.keyboard\hotkeys.ahk"
 
-function RunningAsAdmin {  
+
+$HOTKEYS = "$PWD\.keyboard\hotkeys.ahk"                                                 # Where the hotkeys are stored in the os-setup project
+$STARTUP = "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"        # Where hotkeys.ahk are moved to affect computer
+
+
+function ParentDir($path) {
+    return [System.IO.Path]::GetDirectoryName($path)
+}
+function RunningAsAdmin() {  
     $user = [Security.Principal.WindowsIdentity]::GetCurrent()
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
@@ -17,13 +24,13 @@ function InstallCustomWindowsHotkeys() {
 
         if(-not(RunningAsAdmin)) {
             PrintRed "Script running w/o admin rights, expect an install elevation prompt..."
-            Start-Sleep .75
+            Start-Sleep 1
         } 
 
         Start-Process -Wait -FilePath "$HOME\Downloads\AutoHotkey-v2.exe" -ArgumentList "/silent", "/Elevate" -PassThru
         
         if(-not(Test-Path "C:\Program Files\AutoHotkey")) {
-            PrintRed "Unable to install AutoHotkeyV2. Please install manually. Will still place .ahk file in Startup folder..."
+            PrintRed "Unable to install AutoHotkeyV2. Aborting...InstallCustomWindowsHotkeys"
             RETURN;
         }
     }
@@ -31,4 +38,20 @@ function InstallCustomWindowsHotkeys() {
     Copy-Item -Path $HOTKEYS -Destination "C:\Users\stasp\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
     PrintGreen "InstallCustomWindowsHotkeys Complete"
+}
+
+function BackupAutoHotkey() {
+    $toBackup = "$STARTUP\hotkeys.ahk"
+
+    if(-not(Test-Path $toBackup)) {  PrintRed "BackupAutoHotkey(): `$toBackup not found. `$toBackup: $toBackup"; RETURN;  }
+
+    try {
+        Copy-Item $toBackup $(ParentDir $HOTKEYS)
+    }
+    catch {
+        if($?) { PrintGreen "BackupAutoHotkey(): Success"; RETURN $true }
+        else {
+            PrintRed "BackupAutoHotkey(): Failure! Reason: $($_.Exception.Message)"
+        }
+    }
 }
